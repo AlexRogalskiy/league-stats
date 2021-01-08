f22a77afce444bee2ba8d92da9c83784ef2e3d4c
ADMSK\AVROGAL1 <avrogal1@mts.ru>
2021-01-08 19:29:21 +0300

    Init repo

---
+++ client/src/store/modules/summoner.js
@@ -0,0 +1,246 @@
+import { axios } from '@/plugins/axios'
+import { createMatchData, createBasicSummonerData, createRecordsData } from '@/helpers/summoner'
+
+export const namespaced = true
+
+export const state = {
+  basic: {
+    account: {},
+    currentSeason: null,
+    matchList: [],
+    ranked: {},
+    seasons: [],
+    status: '',
+  },
+  overview: {
+    matchIndex: 0,
+    matches: [],
+    stats: {},
+    loaded: false,
+    matchesLoading: false,
+  },
+  champions: {
+    list: [],
+    championsLoaded: false
+  },
+  records: {
+    list: [],
+    recordsLoaded: false
+  },
+  live: {
+    match: {},
+    liveLoaded: false,
+    playing: false,
+  },
+}
+
+export const mutations = {
+  BASIC_REQUEST(state) {
+    state.basic.status = 'loading'
+    state.basic.currentSeason = null
+    state.champions.championsLoaded = false
+    state.records.recordsLoaded = false
+    state.overview.loaded = false
+    state.live.liveLoaded = false
+  },
+  CHAMPIONS_NOT_FOUND(state) {
+    state.champions.championsLoaded = false
+  },
+  CHAMPIONS_FOUND(state, { champions }) {
+    state.champions.list = champions
+    state.champions.championsLoaded = true
+  },
+  KEEP_LAST_X_MATCHES(state, number) {
+    state.overview.matchIndex = number
+    state.overview.matches = state.overview.matches.slice(0, number)
+  },
+  LIVE_FOUND(state, { live }) {
+    state.live.match = live
+    state.live.liveLoaded = true
+  },
+  LIVE_LOADING(state) {
+    state.live.playing = true
+    state.live.liveLoaded = false
+  },
+  MATCHES_LOADING(state) {
+    state.overview.matchesLoading = true
+  },
+  MATCHES_FOUND(state, { newMatches, stats }) {
+    state.overview.matchesLoading = false
+    state.overview.matches = [...state.overview.matches, ...newMatches]
+    state.overview.matchIndex += newMatches.length
+    state.overview.stats = stats
+    state.champions.championsLoaded = false
+    state.records.recordsLoaded = false
+  },
+  OVERVIEW_FOUND(state, infos) {
+    state.overview.matches = infos.matches
+    state.overview.matchIndex = infos.matches.length
+    state.overview.stats = infos.stats
+    state.overview.loaded = true
+  },
+  RECORDS_FOUND(state, { records }) {
+    state.records.list = records
+    state.records.recordsLoaded = true
+  },
+  SUMMONER_FOUND(state, infos) {
+    state.basic.account = infos.account
+    state.basic.matchList = infos.matchList
+    state.basic.ranked = infos.ranked
+    state.basic.seasons = infos.seasons.sort((a, b) => b - a)
+    state.basic.status = 'found'
+    state.live.match = infos.current
+    state.live.playing = infos.playing
+  },
+  SUMMONER_NOT_FOUND(state) {
+    state.basic.status = 'error'
+  },
+  SUMMONER_NOT_PLAYING(state) {
+    state.live.match = {}
+    state.live.playing = false
+    state.live.liveLoaded = false
+  },
+  UPDATE_SEASON(state, { season }) {
+    state.basic.currentSeason = season
+
+    state.overview.loaded = false
+    state.champions.championsLoaded = false
+    state.records.recordsLoaded = false
+  },
+}
+
+export const actions = {
+  async basicRequest({ commit, dispatch, rootState }, { summoner, region }) {
+    const regionId = rootState.regionsList[region]
+    commit('BASIC_REQUEST')
+    try {
+      const resp = await axios(({ url: 'summoner/basic', data: { summoner, region: regionId }, method: 'POST' }))
+      if (!resp.data) {
+        dispatch('notification/add', {
+          type: 'error',
+          message: 'Summoner not found.'
+        }, { root: true })
+        return commit('SUMMONER_NOT_FOUND')
+      }
+
+      console.log(`---SUMMONER INFOS ${resp.data.account.name}---`)
+      console.log(resp.data)
+      const infos = createBasicSummonerData(resp.data)
+      commit('SUMMONER_FOUND', infos)
+
+      // Add summoner to recent searches
+      dispatch('settings/addRecentSearch', {
+        name: infos.account.name,
+        icon: infos.account.profileIconId,
+        region,
+      }, { root: true })
+    } catch (error) {
+      if (error.response && error.response.status === 422) {
+        dispatch('notification/add', {
+          type: 'error',
+          message: 'Summoner not found.'
+        }, { root: true })
+      }
+      if (error.message !== 'Summoner changed') {
+        commit('SUMMONER_NOT_FOUND')
+      }
+    }
+  },
+  championsNotLoaded({ commit }) {
+    commit('CHAMPIONS_NOT_FOUND')
+  },
+  async championsRequest({ commit }, queue = null) {
+    const resp = await axios(({ url: 'summoner/champions', data: { puuid: state.basic.account.puuid, queue: queue }, method: 'POST' })).catch(() => { })
+    console.log('---CHAMPIONS---')
+    console.log(resp.data)
+
+    commit('CHAMPIONS_FOUND', { champions: resp.data })
+  },
+  async liveMatchRequest({ commit, rootState }) {
+    commit('LIVE_LOADING')
+    const resp = await axios(({
+      url: 'summoner/live',
+      data: {
+        id: state.basic.account.id,
+        region: rootState.regionsList[rootState.settings.region]
+      },
+      method: 'POST'
+    })).catch(() => { })
+    console.log('---LIVE---')
+    console.log(resp.data)
+
+    if (resp.data) {
+      commit('LIVE_FOUND', { live: resp.data })
+    } else {
+      commit('SUMMONER_NOT_PLAYING')
+    }
+  },
+  async moreMatches({ commit, getters, rootState }) {
+    commit('MATCHES_LOADING')
+
+    const gameIds = getters.filteredMatchList
+      .slice(state.overview.matchIndex, state.overview.matchIndex + 10)
+      .map(({ gameId }) => gameId)
+
+    const resp = await axios(({
+      url: 'match',
+      data: {
+        puuid: state.basic.account.puuid,
+        accountId: state.basic.account.accountId,
+        region: rootState.regionsList[rootState.settings.region],
+        gameIds
+      },
+      method: 'POST'
+    })).catch(() => { })
+    console.log('---MATCHES INFOS---')
+    console.log(resp.data)
+    const newMatches = createMatchData(resp.data.matches)
+    commit('MATCHES_FOUND', { newMatches, stats: resp.data.stats })
+  },
+  async overviewRequest({ commit, rootState }) {
+    const resp = await axios(({
+      url: 'summoner/overview',
+      data: {
+        puuid: state.basic.account.puuid,
+        accountId: state.basic.account.accountId,
+        region: rootState.regionsList[rootState.settings.region],
+      },
+      method: 'POST'
+    })).catch(() => { })
+    console.log('---OVERVIEW---')
+    console.log(resp.data)
+    resp.data.matches = createMatchData(resp.data.matchesDetails)
+    commit('OVERVIEW_FOUND', resp.data)
+  },
+  async recordsRequest({ commit }) {
+    const resp = await axios(({ url: 'summoner/records', data: { puuid: state.basic.account.puuid }, method: 'POST' })).catch(() => { })
+    console.log('---RECORDS---')
+    console.log(resp.data)
+    const records = resp.data ? createRecordsData(resp.data) : {}
+
+    commit('RECORDS_FOUND', { records })
+  },
+  sliceOverviewMatches({ commit }, number) {
+    commit('KEEP_LAST_X_MATCHES', number)
+  },
+  updateSeason({ commit }, season) {
+    commit('UPDATE_SEASON', { season })
+  }
+}
+
+export const getters = {
+  filteredMatchList: (state, getters) => {
+    return state.basic.matchList
+      .filter(match => !getters.regionFilterApplied || match.seasonMatch === state.basic.currentSeason)
+  },
+  matchesLoading: state => state.overview.matchesLoading,
+  moreMatchesToFetch: (state, getters) => {
+    return state.overview.matchIndex < getters.filteredMatchList.length
+  },
+  overviewLoaded: state => state.overview.loaded,
+  playing: state => state.live.playing,
+  regionFilterApplied: state => !!state.basic.currentSeason,
+  summonerFound: state => state.basic.status === 'found',
+  summonerNotFound: state => state.basic.status === 'error',
+  summonerLoading: state => state.basic.status === 'loading',
+}
---
+++ client/src/views/SummonerRecords.vue
@@ -0,0 +1,336 @@
+<template>
+  <div key="records">
+    <template v-if="!recordsLoaded || (recordsLoaded && records.maxKda)">
+      <div
+        class="relative pl-6 text-2xl text-blue-200 border-b-2 border-blue-800 category blue-900"
+      >Basics</div>
+      <div class="flex flex-wrap -mx-2">
+        <template v-if="recordsLoaded">
+          <RecordCard
+            color="#63b3ed"
+            text-color="text-blue-400"
+            border-color="border-blue-400"
+            property="kda"
+            :record="records.maxKda"
+            title="KDA"
+          />
+          <RecordCard
+            color="#68D391"
+            text-color="text-green-400"
+            border-color="border-green-400"
+            property="kills"
+            :record="records.maxKills"
+            title="Kills"
+          />
+          <RecordCard
+            color="#9F7AEA"
+            text-color="text-purple-500"
+            border-color="border-purple-500"
+            property="assists"
+            :record="records.maxAssists"
+            title="Assists"
+          />
+          <RecordCard
+            color="#F56565"
+            text-color="text-red-500"
+            border-color="border-red-500"
+            property="deaths"
+            :record="records.maxDeaths"
+            title="Deaths"
+          />
+          <RecordCard
+            color="#D69E2E"
+            text-color="text-yellow-600"
+            border-color="border-yellow-600"
+            property="gold"
+            :record="records.maxGold"
+            title="Gold earned"
+          />
+          <RecordCard
+            color="#81E6D9"
+            text-color="text-teal-300"
+            border-color="border-teal-300"
+            property="minions"
+            :record="records.maxMinions"
+            title="Minions killed"
+          />
+        </template>
+        <template v-else>
+          <div
+            v-for="index in 6"
+            :key="index"
+            style="width: 176px; height: 294px;"
+            class="mx-2 mt-6"
+          >
+            <content-loader
+              :height="294"
+              :width="176"
+              :speed="2"
+              primary-color="#17314f"
+              secondary-color="#2b6cb0"
+            >
+              <rect x="0" y="0" rx="8" ry="8" width="176" height="294" />
+            </content-loader>
+          </div>
+        </template>
+      </div>
+      <div class="relative pl-6 mt-3 text-2xl text-blue-200 border-b-2 border-blue-800 blue-900 category">Game impact</div>
+      <div class="flex flex-wrap -mx-2">
+        <template v-if="recordsLoaded">
+          <RecordCard
+            color="#FC8181"
+            text-color="text-red-400"
+            border-color="border-red-400"
+            property="dmgChamp"
+            :record="records.maxDmgChamp"
+            title="Damage champions"
+          />
+          <RecordCard
+            color="#D69E2E"
+            text-color="text-yellow-400"
+            border-color="border-yellow-400"
+            property="dmgObj"
+            :record="records.maxDmgObj"
+            title="Damage objectives"
+          />
+          <RecordCard
+            color="#FC8181"
+            text-color="text-red-400"
+            border-color="border-red-400"
+            property="dmgTaken"
+            :record="records.maxDmgTaken"
+            title="Damage taken"
+          />
+          <RecordCard
+            v-if="records.maxTowers"
+            color="#D69E2E"
+            text-color="text-yellow-400"
+            border-color="border-yellow-400"
+            property="towers"
+            :record="records.maxTowers"
+            title="Towers"
+          />
+          <RecordCard
+            color="#68D391"
+            text-color="text-green-400"
+            border-color="border-green-400"
+            property="kp"
+            :record="records.maxKp"
+            title="Kill participation"
+          />
+          <RecordCard
+            color="#D69E2E"
+            text-color="text-yellow-400"
+            border-color="border-yellow-400"
+            property="vision"
+            :record="records.maxVision"
+            title="Vision score"
+          />
+        </template>
+        <template v-else>
+          <div
+            v-for="index in 6"
+            :key="index"
+            style="width: 176px; height: 294px;"
+            class="mx-2 mt-6"
+          >
+            <content-loader
+              :height="294"
+              :width="176"
+              :speed="2"
+              primary-color="#17314f"
+              secondary-color="#2b6cb0"
+            >
+              <rect x="0" y="0" rx="8" ry="8" width="176" height="294" />
+            </content-loader>
+          </div>
+        </template>
+      </div>
+      <div class="relative pl-6 mt-3 text-2xl text-blue-200 border-b-2 border-blue-800 category">Miscellaneous</div>
+      <div class="flex flex-wrap -mx-2">
+        <template v-if="recordsLoaded">
+          <RecordCard
+            color="#4299E1"
+            text-color="text-blue-500"
+            border-color="border-blue-500"
+            property="time"
+            :record="records.maxTime"
+            title="Longest game"
+          />
+          <RecordCard
+            v-if="records.maxLiving"
+            color="#4299E1"
+            text-color="text-blue-500"
+            border-color="border-blue-500"
+            property="longestLiving"
+            :record="records.maxLiving"
+            title="Longest living"
+          />
+          <RecordCard
+            v-if="records.maxCriticalStrike"
+            color="#D69E2E"
+            text-color="text-yellow-400"
+            border-color="border-yellow-400"
+            property="criticalStrike"
+            :record="records.maxCriticalStrike"
+            title="Critical Strike"
+          />
+          <RecordCard
+            v-if="records.maxHeal"
+            color="#68D391"
+            text-color="text-green-400"
+            border-color="border-green-400"
+            property="heal"
+            :record="records.maxHeal"
+            title="Heal"
+          />
+        </template>
+        <template v-else>
+          <div
+            v-for="index in 4"
+            :key="index"
+            style="width: 176px; height: 294px;"
+            class="mx-2 mt-6"
+          >
+            <content-loader
+              :height="294"
+              :width="176"
+              :speed="2"
+              primary-color="#17314f"
+              secondary-color="#2b6cb0"
+            >
+              <rect x="0" y="0" rx="8" ry="8" width="176" height="294" />
+            </content-loader>
+          </div>
+        </template>
+      </div>
+      <div v-if="records.maxDouble" class="relative pl-6 mt-3 text-2xl text-blue-200 border-b-2 border-blue-800 category">Multi kills</div>
+      <div v-if="records.maxDouble" class="flex flex-wrap -mx-2">
+        <template v-if="recordsLoaded">
+          <RecordCard
+            color="#FEFCBF"
+            text-color="text-yellow-200"
+            border-color="border-yellow-200"
+            property="doubleKills"
+            :record="records.maxDouble"
+            title="Double kills"
+          />
+          <RecordCard
+            color="#F6E05E"
+            text-color="text-yellow-400"
+            border-color="border-yellow-400"
+            property="tripleKills"
+            :record="records.maxTriple"
+            title="Triple kills"
+          />
+          <RecordCard
+            color="#D69E2E"
+            text-color="text-yellow-600"
+            border-color="border-yellow-600"
+            property="quadraKills"
+            :record="records.maxQuadra"
+            title="Quadra kills"
+          />
+          <RecordCard
+            color="#F56565"
+            text-color="text-red-500"
+            border-color="border-red-500"
+            property="pentaKills"
+            :record="records.maxPenta"
+            title="Penta kills"
+          />
+          <RecordCard
+            color="#63b3ed"
+            text-color="text-blue-400"
+            border-color="border-blue-400"
+            property="killingSpree"
+            :record="records.maxKillingSpree"
+            title="Killing Spree"
+          />
+        </template>
+        <template v-else>
+          <div
+            v-for="index in 5"
+            :key="index"
+            style="width: 176px; height: 294px;"
+            class="mx-2 mt-6"
+          >
+            <content-loader
+              :height="294"
+              :width="176"
+              :speed="2"
+              primary-color="#17314f"
+              secondary-color="#2b6cb0"
+            >
+              <rect x="0" y="0" rx="8" ry="8" width="176" height="294" />
+            </content-loader>
+          </div>
+        </template>
+      </div>
+    </template>
+    <template v-if="recordsLoaded && !records.maxKda">
+      <div class="flex flex-col items-center mt-4">
+        <div>No records have been found.</div>
+        <div>😕</div>
+      </div>
+    </template>
+  </div>
+</template>
+
+<script>
+import { mapActions, mapGetters, mapState } from 'vuex'
+import { ContentLoader } from 'vue-content-loader'
+import RecordCard from '@/components/Summoner/Records/RecordCard.vue'
+
+export default {
+  components: {
+    ContentLoader,
+    RecordCard,
+  },
+
+  computed: {
+    ...mapGetters('summoner', ['summonerFound']),
+    ...mapState({
+      records: state => state.summoner.records.list,
+      recordsLoaded: state => state.summoner.records.recordsLoaded,
+    })
+  },
+
+  watch: {
+    recordsLoaded() {
+      this.fetchData()
+    },
+    summonerFound() {
+      this.fetchData()
+    }
+  },
+
+  created() {
+    this.fetchData()
+  },
+
+  methods: {
+    fetchData() {
+      if (!this.recordsLoaded && this.summonerFound) {
+        this.recordsRequest()
+      }
+    },
+    ...mapActions('summoner', ['recordsRequest']),
+  },
+
+  metaInfo() {
+    return {
+      title: 'Summoner Records',
+    }
+  }
+}
+</script>
+
+<style scoped>
+.category:before {
+  @apply w-2 h-2 bg-blue-200 absolute block left-0 ml-1;
+  content: "";
+  top: 35%;
+  transform: rotate(45deg);
+}
+</style>
